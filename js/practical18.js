df9e6b0ce0b577f9570847bb479b7353fd760be6
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-11-30 20:51:46 +0530

    adding js and regex

--- js/practical18.js
+++ js/practical18.js
@@ -4,3 +4,72 @@ const email = document.getElementById("email");
 const pwd1 = document.getElementById("password");
 const pwd2 = document.getElementById("password2");
 const small = document.getElementById("small");
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();  
+   checkInput();
+});
+function checkInput() {
+
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const pwd1Value = pwd1.value.trim();
+    const pwd2Value = pwd2.value.trim();
+
+    if (usernameValue === "") {
+        showError(username, "Username cannot be blank");
+    }
+    else if (!isValid(usernameValue)) {
+        showError(username, "Username is not valid");
+    }
+    else{
+        showSuccess(username);
+    }
+
+    if (emailValue === "") {
+        showError(email, "Email ID cannot be blank");
+    }
+    else if (!isEmailValid(emailValue)) {
+        showError(email, "Email ID is not valid")
+    }
+    else{
+        showSuccess(email);
+    }
+
+    if (pwd1Value === "") {
+        showError(pwd1, "Password cannot be blank");
+    }
+    else{
+        showSuccess(pwd1);
+    }
+
+    if (pwd2Value === "") {
+        showError(pwd2, "Password cannot be blank");
+    }
+    else if (pwd1Value != pwd2Value) {
+        showError(pwd2, "Password not matched");
+    }
+    else{
+        showSuccess(pwd2);
+    }
+}
+
+function showError(input, msg) {
+    const formControl = input.parentNode;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerHTML = msg;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentNode;
+    formControl.className = 'form-control success';
+}
+
+function isEmailValid(email) {
+    return /^([a-zA-Z0-9_\.\-]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
+}
+
+function isValid(username) {
+    return /^[a-z0-9]{4,24}$/.test(username);
+}
