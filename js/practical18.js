770409bcb6e69179b26bf43f3feae1a475d3b7be
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-11-30 20:42:12 +0530

    editing js of practical18

---
+++ js/practical18.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const pwd1 = document.getElementById("password");
+const pwd2 = document.getElementById("password2");
+const small = document.getElementById("small");
